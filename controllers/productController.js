c512a4bb0f47a5854c5520e5c8b709b8a77aeb2a
Eissanoor <eissaanoor@gmail.com>
2025-05-04 16:21:56 +0500

    updated

--- controllers/productController.js
+++ controllers/productController.js
@@ -99,39 +99,12 @@ exports.getProducts = async (req, res) => {
       query = query.sort('-createdAt');
     }
     
-    // Pagination
-    const page = parseInt(req.query.page, 10) || 1;
-    const limit = parseInt(req.query.limit, 10) || 10;
-    const startIndex = (page - 1) * limit;
-    const endIndex = page * limit;
-    const total = await Product.countDocuments(baseQuery);
-    
-    query = query.skip(startIndex).limit(limit);
-    
     // Executing query
     const products = await query;
     
-    // Pagination result
-    const pagination = {};
-    
-    if (endIndex < total) {
-      pagination.next = {
-        page: page + 1,
-        limit
-      };
-    }
-    
-    if (startIndex > 0) {
-      pagination.prev = {
-        page: page - 1,
-        limit
-      };
-    }
-
     res.status(200).json({
       success: true,
       count: products.length,
-      pagination,
       data: products
     });
   } catch (error) {
